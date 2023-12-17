9e354bbd55ca08ef7aa8af529a53d32f2720e56d
ShadowCrafter011 <lkoe@bluewin.ch>
2023-12-17 20:59:25 +0000

    Added random image command and fixed some stuff

--- cogs/icon.py
+++ cogs/icon.py
@@ -1,5 +1,7 @@
 from discord.ext import tasks, commands
+from discord import app_commands, File
 from constants import constant
+from random import randint
 from print import print
 import pickle
 import os
@@ -18,8 +20,11 @@ class IconHandler(commands.Cog):
 
     @tasks.loop(hours=24.0)
     async def changer(self):
-        with open("data/icon.p", "rb") as pickle_index:
-            icon_index = pickle.loads(pickle_index.read())
+        if os.path.exists("data/icon.p"):
+            with open("data/icon.p", "rb") as pickle_index:
+                icon_index = pickle.loads(pickle_index.read())
+        else:
+            icon_index = 0
 
         icons = list(os.listdir("data/icons"))
         guild = await self.bot.fetch_guild(constant.guild_id)
@@ -34,6 +39,14 @@ class IconHandler(commands.Cog):
         
         with open("data/icon.p", "wb") as pickle_index:
             pickle_index.write(pickle.dumps(icon_index))
+
+    @app_commands.command(name="image", description="Send a random image also used as server icon")
+    async def image(self, interaction):
+        images = os.listdir("data/icons")
+        image = images[randint(0, len(images) - 1)]
+        with open(os.path.join("data/icons", image), "rb") as img:
+            pic = File(img)
+            await interaction.response.send_message(file=pic)
 
     
 async def setup(client):
--- cogs/voice.py
+++ cogs/voice.py
@@ -1,32 +1,61 @@
 from discord.ext import commands
 from discord import app_commands
 import discord
+from pytube import YouTube
+import os
+import time
+
+
+class YT(YouTube):
+    def __init__(self, *args, **kwargs):
+        super(YT, self).__init__(*args, **kwargs)
+
+    async def get_file_path(self, interaction):
+        existing_file = list(filter(lambda path: path.startswith(self.video_id), os.listdir("/home/lukas/WALL-E/data/audio")))
+
+        if len(existing_file) > 0:
+            return os.path.join("/home/lukas/WALL-E/data/audio", existing_file[0])
+        
+        original_response = await interaction.original_response()
+        
+        await original_response.edit(content="Downloading audio file")
+
+        stream = sorted(self.streams.filter(only_audio=True), key=lambda stream: int(stream.abr.replace("kbps", "")), reverse=True)[0]
+        filename = f"{self.video_id}-{time.time()}.webm"
+        stream.download(output_path="/home/lukas/WALL-E/data/audio", filename=filename)
+        return os.path.join("/home/lukas/WALL-E/data/audio", filename)
 
 
 class VoiceHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @app_commands.command(name="play", description="Play the audio of a Youtube video")
+    @app_commands.describe(url="Play the audio of the given URL")
+    async def play(self, interaction, url: str):
+        yt = YT(url)
+        await interaction.response.send_message("Checking whether audio was already downloaded")
+        path = await yt.get_file_path(interaction)
+        self.vc.play(discord.FFmpegPCMAudio(source=path))
+        await (await interaction.original_response()).edit(content=f"Playing {yt.title}, at {path}")
+
     @app_commands.command(name="join", description="Lass WALL-E deinem Sprachchat beitreten")
     async def join(self, interaction):
         if channel := interaction.user.voice.channel:
 
             try:
-                vc = await channel.connect()
+                self.vc = await channel.connect(self_deaf=True)
             except discord.ClientException:
                 return await interaction.response.send_message("WALL-E ist schon in einem Sprachchat")
-            
-            vc.play(discord.FFmpegPCMAudio(source="data/sounds/rick.mp3"))
 
             await interaction.response.send_message("WALL-E ist deinem Sprachchat beigetreten")
         else:
-            await interaction.response.send_message("Du bist in keinem Sprachchat drinnen")
+            await interaction.response.send_message("Du bist mit keinem Sprachchat verbunden")
 
     @app_commands.command(name="leave", description="Lass WALL-E den Sprachchat verlassen")
     async def leave(self, interaction):
-        for voice in self.bot.voice_clients:
-            await voice.disconnect()
-        await interaction.response.send_message("WALL-E hat alle Sprachchats verlassen")
+        await self.vc.disconnect()
+        await interaction.response.send_message("WALL-E den Sprachchat verlassen")
 
 
 async def setup(client):
