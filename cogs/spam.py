42be35da7313ce2a1463fb32a1d7ee46a7a67aa1
ShadowCrafter011 <lkoe@bluewin.ch>
2023-12-18 10:36:28 +0000

    Added spam command

---
+++ cogs/spam.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from discord import app_commands
+
+
+class SpamHandler(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="spam", description="Send a message multiple times")
+    @app_commands.describe(message="Message to be sent", n="Number of times message gets sent capped to 10")
+    async def spam(self, interaction, message:str, n: int):
+        await interaction.response.send_message(f"Sending \"{message}\" {n} times")
+        n = min(n, 10)
+        for _ in range(n):
+            await interaction.channel.send(message)
+
+
+async def setup(client):
+    await client.add_cog(SpamHandler(client))
